5469ece725bf493b0e9ea433f22ecbfb3da9492f
coarf <coarf@hotmail.com>
2023-04-20 13:15:18 -0500

    cambiar el mensaje y crear un Scanner

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hola Clase diseño de sistema");
+        Scanner leer = new Scanner(System.in);
     }
 }
